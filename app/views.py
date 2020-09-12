81f728c83696cc06d6fe0f5e2e5ed08f8784aa59
VickramMS <mowlisri123@gmail.com>
2020-09-12 14:08:48 +0530

    9

--- app/views.py
+++ app/views.py
@@ -10,10 +10,12 @@ import json
 @csrf_exempt
 def SendMail(request):
     resp = json.loads(request.body)
-    if resp["payload"]["payment"]["entity"]["email"] == "authorized":
+    if resp["payload"]["payment"]["entity"]["status"] == "authorized":
         print(resp["payload"]["payment"]["entity"]["email"])
     else:
         print("Payment failed!")
+
+    
     # Get data/json in post
     # Parse the data
     # content = {"name": name,}
