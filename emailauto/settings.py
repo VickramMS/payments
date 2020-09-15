9a281f5afc3bdd308b8ce652c6b191f5f2ff9918
VickramMS <mowlisri123@gmail.com>
2020-09-15 14:43:06 +0530

    Password

--- emailauto/settings.py
+++ emailauto/settings.py
@@ -106,7 +106,7 @@ EMAIL_HOST = 'smtp-relay.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'connect@skillsafari.in'
-EMAIL_HOST_PASSWORD = 'SkillSafari@Edukeys'
+EMAIL_HOST_PASSWORD = 'Edukeys@789'
 
 
 # Internationalization
