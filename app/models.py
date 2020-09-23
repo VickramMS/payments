77cc1055c3a19e12d23f4b686829cc3c8f9deb8a
VickramMS <46955658+VickramMS@users.noreply.github.com>
2020-09-23 20:52:57 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class Record(models. Model):
     email = models.EmailField(max_length=254)
     number = models.CharField(max_length=12)
     whatsapp = models.CharField(max_length=12)
+    time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
